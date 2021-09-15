c4f484e497e3afbd3366fe0705d7773fae1ec619
NickWest273 <89940706+NickWest273@users.noreply.github.com>
2021-09-15 14:44:14 -0400

    Initial commit

---
+++ src/LineSplitExample.java
@@ -0,0 +1,23 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.io.IOException;
+public class LineSplitExample {
+    public static void main(String[] args) throws IOException{
+        var fileName = "jersey.txt";
+        var studentJerseys = Paths.get(fileName);
+        var studentNames = new ArrayList<String>();
+        var jerseyNumbers = new ArrayList<Integer>();
+        var studentLines  = Files.readAllLines(studentJerseys);
+        for (var line : studentLines){
+            var splitLine = line.split(",");
+            studentNames.add(splitLine[0]);
+            var jerseyNum = Integer.parseInt(splitLine[1]);
+            jerseyNumbers.add(jerseyNum);
+
+        }
+        for (int i = 0; i< studentNames.size(); i++){
+            System.out.println("Student: "+ studentNames.get(i) + " Jersey Number: " + jerseyNumbers.get(i));
+        }
+    }
+}
